45cb1c439ae2834a0fc85f55fc0118e340be7ce7
JIAH-LEA <leeahjiahlea@sookmyung.ac.kr>
2021-04-11 15:22:57 +0900

    feat: [Review] ReviewController 추가

--- backend/src/main/java/com/project/backend/review/api/ReviewController.java
+++ backend/src/main/java/com/project/backend/review/api/ReviewController.java
@@ -1,18 +1,42 @@
 package com.project.backend.review.api;
 
+import com.project.backend.review.dto.ReviewCreateRequest;
+import com.project.backend.review.dto.ReviewResponse;
+import com.project.backend.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 
 @RequiredArgsConstructor
+@RequestMapping("/review")
 @RestController
 
 public class ReviewController {
+    public static final String REVIEW_URI = "/review";
+    private final ReviewService reviewService;
 
+    @PostMapping
+    public ResponseEntity<Void> createReview(@RequestBody ReviewCreateRequest request){
+        Long rid = reviewService.create(request);
+        return ResponseEntity
+                .created(URI.create(REVIEW_URI+"/"+rid))
+                .build();
+    }
 
+    @GetMapping("/{rid}")
+    public ResponseEntity<ReviewResponse> findReviewById(
+            @PathVariable Long rid) {
+        ReviewResponse reviewResponse = reviewService.findReviewById(rid);
+        return ResponseEntity.ok(reviewResponse);
+    }
 
-
-
-
-
+    @GetMapping("/all")
+    public ResponseEntity<List<ReviewResponse>> findAllReviews() {
+        List<ReviewResponse> reviewResponses = reviewService.findAllReviews();
+        return ResponseEntity.ok(reviewResponses);
+    }
 }
