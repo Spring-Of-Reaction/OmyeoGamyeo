8b27a13d5d7102c17b150daeec6d12a66ab63ead
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-04-16 00:18:54 +0900

    test: [Post] 수정, 조회 테스트

--- backend/src/test/java/com/project/backend/domain/PostControllerTest.java
+++ backend/src/test/java/com/project/backend/domain/PostControllerTest.java
@@ -3,12 +3,16 @@ package com.project.backend.domain;
 import com.project.backend.post.domain.entity.Post;
 import com.project.backend.post.domain.repository.PostRepository;
 import com.project.backend.post.dto.PostSaveRequestDto;
+import com.project.backend.post.dto.PostUpdateRequestDto;
+import org.junit.After;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -26,10 +30,15 @@ public class PostControllerTest {
     private int port;
 
     @Autowired
-    private TestRestTemplate resttemplate;
+    private TestRestTemplate restTemplate;
 
     @Autowired
     private PostRepository postRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        postRepository.deleteAll();
+    }
 
     @Test
     public void Post_등록() throws Exception {
@@ -58,7 +67,7 @@ public class PostControllerTest {
 
         //when
 
-        ResponseEntity<Long> responseEntity = resttemplate.postForEntity(url, requestDto, Long.class);
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
         //then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -66,6 +75,53 @@ public class PostControllerTest {
         List<Post> all = postRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void Post_수정() throws Exception {
+        //given
+        Post savedPost = postRepository.save(Post.builder()
+                .nickname("nickname")
+                .title("title")
+                .content("content")
+                .category(1)
+                .views(1)
+                .filename("filename")
+                .filepath("filepath")
+                .date(LocalDate.now())
+                .build());
+
+        Long updateId = savedPost.getPid();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+
+        PostUpdateRequestDto requestDto = PostUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+
+        String url = "http://localhost:" + port + "/api/v1/post/" + updateId;
+
+        HttpEntity<PostUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+            //System.out.println(requestEntity); -> <com.project.backend.post.dto.PostUpdateRequestDto@23f2f22d,[]>
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+            /*exchange : HTTP 메서드, URL, 헤더 및 본문을 포함한 RequestEntity를 입력받아 ResponseEntity를 리턴 -> Class 대신 ParameterizedTypeReference 사용하여 제네릭타입 응답 지정 가능*/
+            //System.out.println(responseEntity); -> <200,1,[Content-Type:"application/json", Transfer-Encoding:"chunked", Date:"Thu, 15 Apr 2021 15:14:31 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]>
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Post> all = postRepository.findAll();
+        System.out.println(savedPost.getTitle());
+
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
 
     }
 
