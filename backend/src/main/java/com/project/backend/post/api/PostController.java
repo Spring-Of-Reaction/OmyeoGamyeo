a7d371ab3a6af246236b77a9ae3287184e4c1be4
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-04-21 22:06:42 +0900

    fix: [Post] PostController API url 수정

--- backend/src/main/java/com/project/backend/post/api/PostController.java
+++ backend/src/main/java/com/project/backend/post/api/PostController.java
@@ -15,35 +15,35 @@ public class PostController {
 
     private final PostService postService;
 
-    @PostMapping("/api/v1/post")
+    @PostMapping("/api/post")
     public Long save(@RequestBody PostSaveRequestDto requestDto){
 
         return postService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/post/{pid}")
+    @PutMapping("/api/post/{pid}")
     public Long update(@PathVariable Long pid, @RequestBody PostUpdateRequestDto requestDto){
         return postService.update(pid, requestDto);
     }
 
-    @GetMapping("/api/v1/post/{pid}")
+    @GetMapping("/api/post/{pid}")
     public PostResponseDto findById(@PathVariable Long pid){
         return postService.findById(pid);
     }
 
-    @DeleteMapping("/api/v1/post/{pid}")
+    @DeleteMapping("/api/post/{pid}")
     public Long delete(@PathVariable Long pid){
         postService.delete(pid);
         return pid;
     }
 
-    @GetMapping("/api/v1/post")
+    @GetMapping("/api/post")
     public List list(){
         return postService.findAllDesc();
     }
 
     //category별 1:일상, 2:질문
-    @GetMapping("/api/v1/post/category/{category}")
+    @GetMapping("/api/post/category/{category}")
     public List list_category(@PathVariable Integer category) { return postService.findByCategory(category);}
 
 
