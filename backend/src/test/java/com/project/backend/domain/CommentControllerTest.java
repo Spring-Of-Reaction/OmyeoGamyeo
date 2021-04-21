bd12143fd277f596137d5661e0d3ad60d6aa7376
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-04-22 00:05:38 +0900

    test: [Post] 댓글 삭제 테스트

--- backend/src/test/java/com/project/backend/domain/CommentControllerTest.java
+++ backend/src/test/java/com/project/backend/domain/CommentControllerTest.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.util.Objects.nonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -79,12 +81,56 @@ public class CommentControllerTest {
         Comment comment = commentList.get(0);
         assertThat(comment.getC_content()).isEqualTo(c_content);
 
+        }
 
 
+    @Test
+    public void comment_삭제() {
+        //given
+        String nickname = "테스트 닉네임";
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        Integer category = 1;
+        Integer views = 1;
+        String filename = "테스트 파일이름";
+        String filepath = "테스트 파일경로";
+        LocalDate date = LocalDate.now();
 
+        postRepository.save(Post.builder()
+                .nickname(nickname)
+                .title(title)
+                .content(content)
+                .category(category)
+                .views(views)
+                .filename(filename)
+                .filepath(filepath)
+                .date(date)
+                .build());
 
+        List<Post> postList = postRepository.findAll();
+        String c_content = "댓글1";
+        String c_writer = "글쓴이1";
+        Post post = postList.get(0);
+        LocalDateTime dateTime = LocalDateTime.now();
 
+        commentRepository.save(Comment.builder()
+                .c_content(c_content)
+                .c_writer(c_writer)
+                .post(post)
+                .dateTime(dateTime)
+                .build());
+
+        //when
+        List<Comment> commentList = commentRepository.findAll();
+        Comment testcomment = commentList.get(0);
+
+        assertThat(nonNull(testcomment)); //comment 객체 있었음
+
+        String url = "http://localhost:" + port + "/api/post/1/comment" + testcomment.getCid();
+
+        restTemplate.delete(url, HttpMethod.DELETE, testcomment);
+        //then
+        assertThat(commentRepository.findAll().isEmpty()); //comment 객체 삭제되었음
     }
-
 
 }
