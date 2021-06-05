839a21cf57c446278cf2a158c08e87ae856f81fb
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-06-06 04:06:32 +0900

    feat: [Scrap] 스크랩한글 모아보기

--- backend/src/main/java/com/project/backend/scrap/api/ScrapController.java
+++ backend/src/main/java/com/project/backend/scrap/api/ScrapController.java
@@ -1,18 +1,22 @@
 package com.project.backend.scrap.api;
 
+import com.project.backend.review.dto.ReviewListResponse;
 import com.project.backend.scrap.service.ScrapService;
 import com.project.backend.security.domain.entity.User;
 import com.project.backend.security.domain.repository.UserRepository;
 import com.project.backend.security.jwt.JwtTokenProvider;
+import com.project.backend.security.security.CurrentUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @Slf4j
@@ -43,4 +47,11 @@ public class ScrapController {
                 : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
     }
+
+    @GetMapping("api/scrap/mypage")
+    public List<ReviewListResponse> mypage_scrap(@CurrentUser User user){
+        return scrapService.mypageScrap(user);
+    }
+
+
 }
